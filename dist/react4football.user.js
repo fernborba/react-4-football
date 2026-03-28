1ea58e74b5dbbd2ad6752a1df71c85796af0b127
Fernando Borba <58079853+fernborba@users.noreply.github.com>
2026-03-28 11:35:34 -0300

    Update react4football.user.js

--- dist/react4football.user.js
+++ dist/react4football.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         React 4 Football v7.8.10
+// @name         React 4 Football v7.8.11
 // @namespace    http://tampermonkey.net/
-// @version      7.8.10
+// @version      7.8.11
 // @description  React UI for EA WebApp
 // @author       Fernando
 // @match        https://www.ea.com/*/ea-sports-fc/ultimate-team/web-app/*
@@ -11,7 +11,7 @@
 // @updateURL    https://raw.githubusercontent.com/fernborba/react-4-football/main/dist/react4football.user.js
 // @require      https://unpkg.com/react@18/umd/react.production.min.js
 // @require      https://unpkg.com/react-dom@18/umd/react-dom.production.min.js
-// @require      https://raw.githubusercontent.com/fernborba/react-4-football/refs/heads/main/dist/index4.js?v=v7.8.10
+// @require      https://raw.githubusercontent.com/fernborba/react-4-football/refs/heads/main/dist/index4.js?v=v7.8.11
 // ==/UserScript==
 
 (function () {
@@ -37,7 +37,7 @@ body{background-position:center;background-color:#191820;background-repeat:no-re
     obs.observe(document.documentElement, { childList: true, subtree: true });
   }
 
-  const EXPECTED_BUNDLE_VERSION = "v7.8.10";
+  const EXPECTED_BUNDLE_VERSION = "v7.8.11";
   const startupState = {
     failed: false,
     reason: null,
