906f1a311717bfe1f610e3cf2f4193b2995f09ef
Fernando Borba <58079853+fernborba@users.noreply.github.com>
2026-04-08 05:53:18 -0300

    Update react4football.meta.js

---
+++ dist/react4football.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         React 4 Football
+// @namespace    http://tampermonkey.net/
+// @version      7.10.18
+// @description  React UI for EA WebApp
+// @author       Fernando
+// @match        https://www.ea.com/*/ea-sports-fc/ultimate-team/web-app/*
+// @match        https://www.easports.com/*/ea-sports-fc/ultimate-team/web-app/*
+// @grant        unsafeWindow
+// @grant        GM_getValue
+// @downloadURL  https://raw.githubusercontent.com/fernborba/react-4-football/main/dist/react4football.user.js
+// @updateURL    https://raw.githubusercontent.com/fernborba/react-4-football/main/dist/react4football.meta.js
+// @require      https://unpkg.com/react@18/umd/react.production.min.js
+// @require      https://unpkg.com/react-dom@18/umd/react-dom.production.min.js
+// @require      https://raw.githubusercontent.com/fernborba/react-4-football/refs/heads/main/dist/index4.js?v=v7.10.18
+// ==/UserScript==
